6141c72f7454af802099a88d536ad1907d4eb375
jdgualteros <jdgualteros@devops.com>
2022-03-31 19:45:03 -0500

    inicio jenkins maven

---
+++ app/appSpark/src/main/scala/com/oliver/App.scala
@@ -0,0 +1,26 @@
+package com.oliver
+
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+
+  def main(args : Array[String]) {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .getOrCreate()
+
+    spark.read.format("csv").option("header","true").load("/raw/machine.csv").show()
+
+//    val data = spark.sparkContext.parallelize(Seq("Hello World", "This is some text", "Hello text"))
+//    val map = data.flatMap(e => e.split(" ")).map(word => (word, 1))
+//    val counts = map.reduceByKey(_ + _).repartition(1)
+
+//    counts.saveAsTextFile("wordcount")
+  }
+
+}
